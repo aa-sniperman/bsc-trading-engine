dfc0200a0916f8f595daeeabc1e052265128643d
aa-sniperman <nhatducmo@gmail.com>
2025-02-20 22:45:40 +0700

    build: buy sell with bnb on pancake v3

--- src/pancake-v3/swapper.ts
+++ src/pancake-v3/swapper.ts
@@ -11,11 +11,24 @@ export namespace PancakeV3Swap {
     await tx.wait();
     return tx.hash;
   }
+
+  export async function wrapIfNeeded(wallet: Wallet, amount: bigint) {
+    const wNativeBal = await Token.getTokenBalance(wallet.address, WRAPPED_NATIVE);
+    if(wNativeBal >= amount) return;
+    const hash = await wrap(wallet, amount - wNativeBal);
+    console.log(hash);
+  }
   export async function unwrap(wallet: Wallet, amount: bigint) {
     const weth = IWETH__factory.connect(WRAPPED_NATIVE, wallet);
     const tx = await weth.withdraw(amount);
     await tx.wait();
     return tx.hash;
+  }
+
+  export async function unwrapAll(wallet: Wallet) {
+    const wNativeBal = await Token.getTokenBalance(wallet.address, WRAPPED_NATIVE);
+    const hash = await unwrap(wallet, wNativeBal)
+    console.log(hash);
   }
   export async function executeSwap(wallet: Wallet, tokenIn: string, tokenOut: string, amountIn: bigint, fee = 10000, recipient?: string) {
     await Token.approveIfNeeded(wallet, PANCACKE_V3_ROUTER, tokenIn as string, amountIn as bigint);
@@ -32,6 +45,45 @@ export namespace PancakeV3Swap {
       sqrtPriceLimitX96: 0n
     });
     await tx.wait();
+    return tx.hash;
+  }
+
+  export async function buyWithNative(wallet: Wallet, tokenOut: string, amountIn: bigint, fee = 10000, recipient?: string) {
+    await wrapIfNeeded(wallet, amountIn)
+    await Token.approveIfNeeded(wallet, PANCACKE_V3_ROUTER, WRAPPED_NATIVE, amountIn as bigint);
+
+    const routerContract = PancackeV3Router__factory.connect(PANCACKE_V3_ROUTER, wallet);
+    const tx = await routerContract.exactInputSingle({
+      tokenIn: WRAPPED_NATIVE,
+      tokenOut,
+      fee,
+      recipient: recipient ?? wallet.address,
+      deadline: Date.now() + 60000,
+      amountIn,
+      amountOutMinimum: 0n,
+      sqrtPriceLimitX96: 0n
+    });
+    await tx.wait();
+    return tx.hash;
+  }
+
+  export async function sellToNative(wallet: Wallet, tokenIn: string, amountIn: bigint, fee = 10000, recipient?: string) {
+    await Token.approveIfNeeded(wallet, PANCACKE_V3_ROUTER, tokenIn, amountIn as bigint);
+
+    const routerContract = PancackeV3Router__factory.connect(PANCACKE_V3_ROUTER, wallet);
+    const tx = await routerContract.exactInputSingle({
+      tokenIn,
+      tokenOut: WRAPPED_NATIVE,
+      fee,
+      recipient: recipient ?? wallet.address,
+      deadline: Date.now() + 60000,
+      amountIn,
+      amountOutMinimum: 0n,
+      sqrtPriceLimitX96: 0n
+    });
+    await tx.wait();
+    
+    await unwrapAll(wallet);
     return tx.hash;
   }
 }
--- src/waifu.index.ts
+++ src/waifu.index.ts
@@ -2,15 +2,14 @@ import { parseEther, Wallet } from "ethers";
 import { env } from "./configs";
 import { PROVIDER, TokenConfig, WRAPPED_NATIVE } from "./constants";
 import { PancakeV3Swap } from "./pancake-v3/swapper";
+import { Token } from "./token";
 
 async function main() {
     const pk = env.keys.pk;
     const wallet = new Wallet(pk, PROVIDER);
-    const hash = await PancakeV3Swap.wrap(wallet, parseEther('0.001'));
-    console.log(hash);
-    const swapHash = await PancakeV3Swap.executeSwap(wallet, WRAPPED_NATIVE,
-        TokenConfig.WAIFU.address,
-        parseEther('0.001'),
+
+    const swapHash = await PancakeV3Swap.buyWithNative(wallet, TokenConfig.WAIFU.address,
+        parseEther('0.0005'),
         10000,
     )
     console.log(swapHash);
